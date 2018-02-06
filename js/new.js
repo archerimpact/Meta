706a98f504f52c18e9e655c9e5e0bea1df1d0bc6
Franklin Rice <frice@berkeley.edu>
2018-02-05 23:26:19 -0800

    navigation helper function

--- js/new.js
+++ js/new.js
@@ -25,38 +25,40 @@ function createProject(){
 
 $("#new-project").submit(function(e) {
 	e.preventDefault();
+	console.log(e);
 	createProject();
-})
+	redirect('landing')
+});
 
-(function () {
-        var holder = document.getElementById('upload');
-        if (!holder) {
-          return false;
-        }
-
-        holder.ondragover = () => {
-            return false;
-        };
-
-        holder.ondragleave = () => {
-            return false;
-        };
-
-        holder.ondragend = () => {
-            return false;
-        };
-
-        holder.onclick = () => {
-          console.log("hello");
-        }
-
-        holder.ondrop = (e) => {
-            e.preventDefault();
-
-            for (let f of e.dataTransfer.files) {
-                console.log('File(s) you dragged here: ', f.path)
-            }
-
-            return false;
-        };
-    })();
+// (function () {
+//         var holder = document.getElementById('upload');
+//         if (!holder) {
+//           return false;
+//         }
+//
+//         holder.ondragover = () => {
+//             return false;
+//         };
+//
+//         holder.ondragleave = () => {
+//             return false;
+//         };
+//
+//         holder.ondragend = () => {
+//             return false;
+//         };
+//
+//         holder.onclick = () => {
+//           console.log("hello");
+//         }
+//
+//         holder.ondrop = (e) => {
+//             e.preventDefault();
+//
+//             for (let f of e.dataTransfer.files) {
+//                 console.log('File(s) you dragged here: ', f.path)
+//             }
+//
+//             return false;
+//         };
+//     })();
