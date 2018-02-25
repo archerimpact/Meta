75c218f14c482eb53e42fad7839b0b5369f0e055
Franklin Rice <frice@berkeley.edu>
2018-02-24 16:37:31 -0800

    pic and data display working<exclamation>

--- js/detail.js
+++ js/detail.js
@@ -6,6 +6,7 @@ const Image = require('./image.js')
 const getProjectJsonPath = require('./project.js').getProjectJsonPath
 const loadProject = require('./project.js').loadProject
 const Mustache = require('Mustache')
+const ExifImage = require('exif').ExifImage;
 
 function loadDetail(projectName){
 	clearDetailsHtml();
@@ -19,7 +20,14 @@ function loadDetail(projectName){
 	redirect('detail');
   	//document.getElementById('name-header').innerHTML = document.getElementById('name-header').innerHTML.replace("name-pc", projectName).replace("description-pc", "description here");
   	loadHeader(project);
-  	loadImages(project);
+  	//loadImages(project);
+	var images = project.loadImages();
+	console.log(images);
+	for (var key in images) {
+		var img_path = images[key]['path'];
+		var name = images[key]['name'];
+		detailExifDisplay(img_path, name);
+	}
 }
 
 function loadImages(project){
@@ -47,7 +55,7 @@ function loadImages(project){
 				mdata += '<td>' + key + ': ' + data[key] + '</td></tr>';
 				count = 0;
 			}
-			
+
 		}
 
 		var template = [
@@ -78,6 +86,45 @@ function loadImages(project){
 
 }
 
+function insertDetailTemplate(data, id) {
+	mdata = '';
+	count = 0;
+	for (var key in data.exifData) {
+		if (count == 0) {
+			mdata += '<tr><td>' + key + ': ' + data.exifData[key] + '</td>';
+			count = 1;
+		} else {
+			mdata += '<td>' + key + ': ' + data.exifData[key] + '</td></tr>';
+			count = 0;
+		}
+	}
+
+	var template = [
+			'<div id="detail-template" class="row">',
+				'<div class="col-md-4">',
+					'<a href="#">',
+						'<img class="img-fluid rounded mb-3 mb-md-0" src="{{path}}" alt="">',
+					'</a>',
+				'</div>',
+				'<div class="col-md-8">',
+					'<h3>{{name}}</h3>',
+					'<p>',
+					'<div id="metadata' + id +' " class="container collapse">',
+				'<table class="table table-bordered">',
+					mdata,
+				'</table>',
+			'</div>',
+					'<span><button class="btn btn-primary mb-2" data-toggle="collapse" data-target="#metadata' + id + ' ">Metadata</button></span>',
+				'</div>',
+			'</div>',
+			'<hr>'
+	].join("\n");
+	// template: '<div ...>\n<h1 ...>{{title}}<h1>\n</div>'
+
+	var filler = Mustache.render(template, data);
+	$("#image-wrapper").append(filler);
+}
+
 function loadHeader(project) {
   template = [
     "<h1 id='name-header' class='my-4'>{{projName}}",
@@ -96,6 +143,35 @@ function clearDetailsHtml(){
 	// clear previous projects on the html
 	document.getElementById("detail-header").innerHTML = ""
 	document.getElementById("image-wrapper").innerHTML = ""
+}
+
+function detailExifDisplay(imgpath, name) {
+	try {
+		new ExifImage({ image : imgpath }, function (error, exifData) {
+				if (error)
+						console.log('Error: '+error.message);
+				else {
+						var data = {
+							'name': name,
+							'path': imgpath,
+							'exifData': {},
+						};
+						var types = ['exif', 'image', 'gps'];
+						for (var ind in types) {
+							var type = types[ind];
+							for (var key in exifData[type]) {
+								var val = exifData[type][key];
+								if (val.constructor.name === 'Number' || val.constructor.name ==='String') {
+									data['exifData'][key] = val;
+								}
+							}
+						}
+						insertDetailTemplate(data, name);
+				}
+		});
+	} catch (error) {
+			console.log('Exif Error: ' + error.message);
+	}
 }
 
 module.exports = {
