0b063ff2dae9845605dc4e87260dd5c8d79f78f7
= <varshavenkat@berkeley.edu>
2018-04-15 16:56:15 -0700

    updates for correct sqlite connection

--- js/new.js
+++ js/new.js
@@ -2,10 +2,28 @@ const ProjectF = require('./js/project.js')
 const Project = ProjectF.Project
 const loadDetail = require('./js/detail.js').loadDetail
 const os = require('os')
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database(':memory:');
 
 var paths_global = [];
 
 function createProject(){
+	db.serialize(function() {
+	  db.run("CREATE TABLE lorem (info TEXT)");
+
+	  var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
+	  for (var i = 0; i < 10; i++) {
+	      stmt.run("Ipsum " + i);
+	  }
+	  stmt.finalize();
+
+	  db.each("SELECT rowid AS id, info FROM lorem", function(err, row) {
+	      console.log(row.id + ": " + row.info);
+	  });
+	});
+
+	db.close();
+
 	var remote = electron.remote;
 	// var store = remote.getGlobal('sharedObj').store;
 
