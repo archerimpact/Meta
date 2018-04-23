2a87e0e33ddaae8c0e4298e7a445074103f10f99
Jordan Flores <jflores@berkeley.edu>
2018-04-22 21:29:33 -0700

    database class working

--- js/database.js
+++ js/database.js
@@ -1,10 +1,18 @@
 const os = require('os')
 var sqlite3 = require('sqlite3');
-var db_filename = './db/meta.sqlite'
+var db_filename = './meta.db'
 
 class Database {
   constructor(opts) {
     this.db = this.init_database();
+  }
+
+  update_image(name, data_dict_questionmark) {
+    // Check if image has been made yet, if not create it
+
+    // Update each column for row, create column if it didn't exist before
+
+    // Update last_modified field
   }
   
   init_database() {
@@ -14,7 +22,7 @@ class Database {
       }
 
       // if meta.db was just created, create Images and Projects tables.
-      this.create_tables_if_new_db();
+      this.create_tables_if_new_db(db);
 
       console.log('Connected to the meta database.');
     });
@@ -22,16 +30,16 @@ class Database {
     return db;
   }
 
-  create_tables_if_new_db() {
+  create_tables_if_new_db(db) {
     var image_query = "SELECT name FROM sqlite_master WHERE type='table' AND name='Images'";
     var project_query = "SELECT name FROM sqlite_master WHERE type='table' AND name='Projects'";
     var setting_query = "SELECT name FROM sqlite_master WHERE type='table' AND name='Settings'";
 
     // check if Images table exists, and create if it does not
-    this.db.get(image_query, (err, row) => {
+    db.get(image_query, (err, row) => {
       if (row == undefined) {
-        var create_image_table = "CREATE TABLE Images (name TEXT, id INTEGER, creation NUMERIC, update NUMERIC, tags TEXT, starred TEXT, notes TEXT)";
-        this.db.run(create_image_table);
+        var create_image_table = "CREATE TABLE Images (name TEXT, proj_id INTEGER, creation NUMERIC, last_modified NUMERIC, tags TEXT, starred TEXT, notes TEXT)";
+        db.run(create_image_table);
         console.log("create Images");
       } else {
         console.log("Images already exists");
@@ -39,10 +47,10 @@ class Database {
     });
 
     // check if Projects table exists, and create if it does not
-    this.db.get(project_query, (err, row) => {
+    db.get(project_query, (err, row) => {
       if (row == undefined) {
-        var create_project_table = "CREATE TABLE Projects (name TEXT, id INTEGER, description TEXT)";
-        this.db.run(create_project_table);
+        var create_project_table = "CREATE TABLE Projects (name TEXT, proj_id INTEGER, description TEXT, last_modified NUMERIC)";
+        db.run(create_project_table);
         console.log("create Projects");
       } else {
         console.log("Projects already exists");
@@ -50,17 +58,16 @@ class Database {
     });
 
     // check if Settings table exists, and create if it does not
-    this.db.get(setting_query, (err, row) => {
+    db.get(setting_query, (err, row) => {
       if (row == undefined) {
         var create_setting_table = "CREATE TABLE Settings (type TEXT, setting TEXT)";
-        this.db.run(create_setting_table);
+        db.run(create_setting_table);
         console.log("create Settings");
       } else {
         console.log("Settings already exists");
       }
     });
   }
-
 }
 
 module.exports = Database
